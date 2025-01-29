d1e1f733974710a7e2df569e00286dd863ea78af
kamila-iwanska <253027@student.pwr.edu.pl>
2025-01-29 22:09:36 +0100

    Refactor TicketsView to display detailed reservation and transaction information

--- frontend/src/TicketsView.jsx
+++ frontend/src/TicketsView.jsx
@@ -1,22 +1,64 @@
 import React from "react";
 import { Link } from "react-router";
 
+const reservations = [
+  {
+    id: 1,
+    movie: {
+      imageURL: "https://assets.upflix.pl/media/plakat/2017/paddington-2__300_427.jpg", //Reservation->AvailableSeats->Showing->Movie(nie istnieje)
+      title: "Paddington 2",
+    },
+    showingDetails: {
+      //Reservation/AvailableSeats/Showing
+      date: "12213",
+      hour: "123213",
+    },
+    hall: "5",
+    seat: {
+      //Reservation/AvailableSeats/Seatseat
+      row: "3",
+      seats: "3",
+    },
+    price: "29.00",
+  },
+];
+
+const transactions = [
+  {
+    id: 1,
+    movie: {
+      imageURL: "https://fwcdn.pl/fpo/53/51/595351/7662231_1.3.jpg", //Reservation->AvailableSeats->Showing->Movie(nie istnieje)
+      title: "Paddington",
+    },
+    showingDetails: {
+      //Reservation/AvailableSeats/Showing
+      date: "12213",
+      hour: "123213",
+    },
+    hall: "5",
+    seat: {
+      //Reservation/AvailableSeats/Seatseat
+      row: "3",
+      seats: "3",
+    },
+    price: "29.00",
+  },
+];
+
 export function TicketsView() {
-  let reservations = [1, 2];
-  let transactions = [1, 2, 3];
   const listOfReservations = reservations.map((reservation) => (
-    <div className="reservation" key={reservation}>
+    <div className="reservation" key={reservation.id}>
       <div className="movie-poster-div">
-        <img className="movie-poster" src="https://fwcdn.pl/fpo/53/51/595351/7662231_1.3.jpg" alt="Paddington (2014)" />
+        <img className="movie-poster" src={reservation.movie.imageURL} alt={reservation.movie.title} />
       </div>
       <div className="reservation-info">
-        <p className="movie-title">Paddington</p>
-        <p className="movie-date">2021-12-12</p>
-        <p className="movie-hour">12:00</p>
-        <p className="movie-hall">Sala 5</p>
-        <p className="movie-row">Rząd 13</p>
-        <p className="movie-seats">Miejsca: 1, 2, 3</p>
-        <p className="movie-price">Suma: 20 zł</p>
+        <p className="movie-title">{reservation.movie.title}</p>
+        <p className="movie-date">{reservation.showingDetails.date}</p>
+        <p className="movie-hour">{reservation.showingDetails.hour}</p>
+        <p className="movie-hall">Sala: {reservation.hall}</p>
+        <p className="movie-row">Rząd: {reservation.seat.row}</p>
+        <p className="movie-seats">Miejsca: {reservation.seat.seats}</p>
+        <p className="movie-price">Suma: {reservation.price} zł</p>
       </div>
       <div className="reservation-buttons">
         <button type="button" className="pay-button">
@@ -30,23 +72,18 @@ export function TicketsView() {
   ));
 
   const listOfTransactions = transactions.map((transaction) => (
-    <div className="reservation" key={transaction}>
+    <div className="reservation" key={transaction.id}>
       <div className="movie-poster-div">
-        <img className="movie-poster" src="https://fwcdn.pl/fpo/53/51/595351/7662231_1.3.jpg" alt="Paddington (2014)" />
+        <img className="movie-poster" src={transaction.movie.imageURL} alt={transaction.movie.title} />
       </div>
       <div className="reservation-info">
-        <p className="movie-title">Paddington</p>
-        <p className="movie-date">2021-12-12</p>
-        <p className="movie-hour">12:00</p>
-        <p className="movie-hall">Sala 5</p>
-        <p className="movie-row">Rząd 13</p>
-        <p className="movie-seats">Miejsca: 1, 2, 3</p>
-        <p className="movie-price">Suma: 20 zł</p>
-      </div>
-      <div className="reservation-buttons">
-        <button type="button" className="cancel-button">
-          Anuluj
-        </button>
+        <p className="movie-title">{transaction.movie.title}</p>
+        <p className="movie-date">{transaction.showingDetails.date}</p>
+        <p className="movie-hour">{transaction.showingDetails.hour}</p>
+        <p className="movie-hall">Sala: {transaction.hall}</p>
+        <p className="movie-row">Rząd: {transaction.seat.row}</p>
+        <p className="movie-seats">Miejsca: {transaction.seat.seats}</p>
+        <p className="movie-price">Suma: {transaction.price} zł</p>
       </div>
     </div>
   ));
