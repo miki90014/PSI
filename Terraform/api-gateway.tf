bb56544c8540fe1351bc6b202dc3241e4ff6ae26
jakubMorawski <110667020+jakub-begginer@users.noreply.github.com>
2025-01-30 16:49:21 +0100

    api gateway witout authorization

---
+++ Terraform/api-gateway.tf
@@ -0,0 +1,59 @@
+resource "aws_api_gateway_rest_api" "cinema_api" {
+  name        = "cinema-api"
+  description = "REST API gateway for cinema app"
+}
+
+resource "aws_api_gateway_resource" "customer" {
+  rest_api_id = aws_api_gateway_rest_api.cinema_api.id
+  parent_id   = aws_api_gateway_rest_api.cinema_api.root_resource_id
+  path_part   = "customer"
+}
+
+resource "aws_api_gateway_method" "customer_method" {
+  rest_api_id   = aws_api_gateway_rest_api.cinema_api.id
+  resource_id   = aws_api_gateway_resource.customer.id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "customer_integration" {
+  rest_api_id = aws_api_gateway_rest_api.cinema_api.id
+  resource_id = aws_api_gateway_resource.customer.id
+  http_method = aws_api_gateway_method.customer_method.http_method
+  type        = "HTTP_PROXY"
+  uri         = "http://${aws_lb.chat_alb.dns_name}/customer"
+}
+
+resource "aws_api_gateway_resource" "employee" {
+  rest_api_id = aws_api_gateway_rest_api.cinema_api.id
+  parent_id   = aws_api_gateway_rest_api.cinema_api.root_resource_id
+  path_part   = "employee"
+}
+
+resource "aws_api_gateway_method" "employee_method" {
+  rest_api_id   = aws_api_gateway_rest_api.cinema_api.id
+  resource_id   = aws_api_gateway_resource.employee.id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "employee_integration" {
+  rest_api_id = aws_api_gateway_rest_api.cinema_api.id
+  resource_id = aws_api_gateway_resource.employee.id
+  http_method = aws_api_gateway_method.employee_method.http_method
+  type        = "HTTP_PROXY"
+  uri         = "http://${aws_lb.chat_alb.dns_name}/employee"
+}
+
+resource "aws_api_gateway_deployment" "cinema_api_deployment" {
+  depends_on = [
+    aws_api_gateway_integration.customer_integration,
+    aws_api_gateway_integration.employee_integration,
+  ]
+  rest_api_id = aws_api_gateway_rest_api.cinema_api.id
+}
+
+output "api_gateway_url" {
+  value = aws_api_gateway_deployment.example.invoke_url
+}
+
