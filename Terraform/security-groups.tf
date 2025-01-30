ca52f6f7004de51724d2b66bd8d24e199bb93bf8
jakubMorawski <110667020+jakub-begginer@users.noreply.github.com>
2025-01-30 17:14:57 +0100

    security groups

---
+++ Terraform/security-groups.tf
@@ -0,0 +1,55 @@
+resource "aws_security_group" "rds-sg" {
+  name        = "rds-sg"
+  description = "Allow traffic to RDS"
+  vpc_id      = aws_vpc.cinema_vpc.id
+
+  ingress {
+    from_port       = 5432
+    to_port         = 5432
+    protocol        = "tcp"
+    security_groups = [aws_security_group.backend-sg.id]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "backend-sg" {
+  name        = "backend-sg"
+  description = "Allow traffic to backend"
+  vpc_id      = aws_vpc.cinema_vpc.id
+
+  ingress {
+    from_port       = 3000
+    to_port         = 3000
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "alb_sg" {
+  vpc_id = aws_vpc.cinema_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
