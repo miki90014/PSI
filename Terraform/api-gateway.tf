8c5c5ff3da5ace8bd5e114d8c0aacf3c70fb5e9c
jakubMorawski <110667020+jakub-begginer@users.noreply.github.com>
2025-01-30 19:53:40 +0100

    api gateway without authorization yet

--- Terraform/api-gateway.tf
+++ Terraform/api-gateway.tf
@@ -1,3 +1,4 @@
+
 resource "aws_api_gateway_rest_api" "cinema_api" {
   name        = "cinema-api"
   description = "REST API gateway for cinema app"
@@ -17,11 +18,14 @@ resource "aws_api_gateway_method" "customer_method" {
 }
 
 resource "aws_api_gateway_integration" "customer_integration" {
-  rest_api_id = aws_api_gateway_rest_api.cinema_api.id
-  resource_id = aws_api_gateway_resource.customer.id
-  http_method = aws_api_gateway_method.customer_method.http_method
-  type        = "HTTP_PROXY"
-  uri         = "http://${aws_lb.cinema_alb.dns_name}/customer"
+  rest_api_id             = aws_api_gateway_rest_api.cinema_api.id
+  resource_id             = aws_api_gateway_resource.customer.id
+  http_method             = aws_api_gateway_method.customer_method.http_method
+  type                    = "HTTP_PROXY"
+  integration_http_method = "ANY"
+  uri                     = "http://${aws_lb.cinema_alb.dns_name}/customer"
+
+  depends_on = [aws_api_gateway_method.customer_method]
 }
 
 resource "aws_api_gateway_resource" "employee" {
@@ -38,22 +42,22 @@ resource "aws_api_gateway_method" "employee_method" {
 }
 
 resource "aws_api_gateway_integration" "employee_integration" {
-  rest_api_id = aws_api_gateway_rest_api.cinema_api.id
-  resource_id = aws_api_gateway_resource.employee.id
-  http_method = aws_api_gateway_method.employee_method.http_method
-  type        = "HTTP_PROXY"
-  uri         = "http://${aws_lb.cinema_alb.dns_name}/employee"
+  rest_api_id             = aws_api_gateway_rest_api.cinema_api.id
+  resource_id             = aws_api_gateway_resource.employee.id
+  http_method             = aws_api_gateway_method.employee_method.http_method
+  integration_http_method = "ANY"
+  type                    = "HTTP_PROXY"
+  uri                     = "http://${aws_lb.cinema_alb.dns_name}/employee"
+
+  depends_on = [aws_api_gateway_method.employee_method]
 }
 
 resource "aws_api_gateway_deployment" "cinema_api_deployment" {
   depends_on = [
+    aws_api_gateway_method.customer_method,
+    aws_api_gateway_method.employee_method,
     aws_api_gateway_integration.customer_integration,
     aws_api_gateway_integration.employee_integration,
   ]
   rest_api_id = aws_api_gateway_rest_api.cinema_api.id
 }
-
-output "api_gateway_url" {
-  value = aws_api_gateway_deployment.cinema_api_deployment.invoke_url
-}
-
