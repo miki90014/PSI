b3a5d681f6b213242b051d22df15bd7c55171164
kamila-iwanska <253027@student.pwr.edu.pl>
2025-02-01 19:02:27 +0100

    Add better frontend

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { cognitoLogin } from "./cognito";
-import { useNavigate } from "react-router";
+import { useNavigate, Link } from "react-router";
 
 export function Login({ setAccessToken }) {
   const [username, setUsername] = useState("");
@@ -8,33 +8,58 @@ export function Login({ setAccessToken }) {
   const navigate = useNavigate();
 
   return (
-    <div className="login-page">
-      <div className="login-form">
-        <h1>Login</h1>
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            console.log({ username, password });
-            cognitoLogin(username, password)
-              .then((result) => {
-                setAccessToken(result.AuthenticationResult.AccessToken);
-                navigate("/client/login/userdata");
-              })
-              .catch((error) => window.alert(error.message));
-          }}
-        >
-          <label htmlFor="username">Username:</label>
-          <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
-          <br />
-          <label htmlFor="password">Password:</label>
-          <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-          <br />
-          <button type="submit">Login</button>
-        </form>
-        <button className="register-btn" onClick={() => navigate("/client/register")}>
-          Register
-        </button>
+    <>
+      <div className="nav-bar">
+        <ul>
+          <li>
+            <Link to="/client">Strona główna</Link>
+          </li>
+          <li>
+            <Link to="/client">Repertuar</Link>
+          </li>
+          <li>
+            <Link to="/client">Nasze kina</Link>
+          </li>
+          <li>
+            <Link className="active" to="/client/login/reservations">
+              Bilety
+            </Link>
+          </li>
+          <li>
+            <Link to="/client/login/userdata">Moje konto</Link>
+          </li>
+        </ul>
       </div>
-    </div>
+      <div className="login-page">
+        <div className="login-form">
+          <h1>Logowanie</h1>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              console.log({ username, password });
+              cognitoLogin(username, password)
+                .then((result) => {
+                  setAccessToken(result.AuthenticationResult.AccessToken);
+                  navigate("/client/login/userdata");
+                })
+                .catch((error) => window.alert(error.message));
+            }}
+          >
+            <label htmlFor="username">Nazwa użytkownika</label>
+            <input type="text" placeholder="Nazwa użytkownika" value={username} onChange={(e) => setUsername(e.target.value)} />
+            <br />
+            <label htmlFor="password">Hasło</label>
+            <input type="password" placeholder="Hasło" value={password} onChange={(e) => setPassword(e.target.value)} />
+            <br />
+            <div className="login-buttons">
+              <button type="submit">Zaloguj</button>
+              <button type="button" className="register-btn" onClick={() => navigate("/client/register")}>
+                Rejestracja
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
   );
 }
