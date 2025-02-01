463891264c945bfa93b56d23b038f81a5597e9aa
jakubMorawski <110667020+jakub-begginer@users.noreply.github.com>
2025-02-01 12:47:22 +0100

    script to automate building

--- python-flask-employee/build_and_push.sh
+++ python-flask-employee/build_and_push.sh
@@ -1,7 +1,15 @@
-docker build --no-cache --platform=linux/amd64 -t dockermaniac01/backend-cinema-image:latest \
-    --build-arg GIT_USER=mxro01 \
-    --build-arg GIT_REPO=devops9-cicd \
-    --secret id=github_token,src=token \
-    .
+docker build --no-cache --platform=linux/amd64 -t dockermaniac01/backend-cinema-image:latest .
 
 
+
+IMAGE_NAME="dockermaniac01/backend-cinema-image"
+TAG_DATE=$(date +%Y%m%d%H%M%S)
+
+docker tag ${IMAGE_NAME}:latest ${IMAGE_NAME}:latest
+docker tag ${IMAGE_NAME}:latest ${IMAGE_NAME}:${TAG_DATE}
+
+docker push ${IMAGE_NAME}:latest
+docker push ${IMAGE_NAME}:${TAG_DATE}
+
+echo "Newest image ${IMAGE_NAME}:${TAG_DATE}"
+echo "Image has been pushed to DockerHub registry"
