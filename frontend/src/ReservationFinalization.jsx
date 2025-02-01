3ec0772e0c9d3a56638da9e33db26d5356b649e7
Monika <tinusia09@gmail.com>
2025-02-01 09:39:31 +0100

    Small fixes in front (#15)

--- frontend/src/ReservationFinalization.jsx
+++ frontend/src/ReservationFinalization.jsx
@@ -1,4 +1,4 @@
-import { useParams, useLocation, useNavigate } from "react-router-dom";
+import { useParams, useLocation, useNavigate, Link } from "react-router-dom";
 import { Container, Row, Col, Spinner, Alert, Form, Button } from "react-bootstrap";
 import React, { useState, useEffect } from "react";
 
@@ -119,6 +119,27 @@ export function ReservationFinalization() {
 
   return (
     <Container>
+      <div className="nav-bar">
+        <ul>
+          <li>
+            <Link to="/client">Strona główna</Link>
+          </li>
+          <li>
+            <Link to="/client">Repertuar</Link>
+          </li>
+          <li>
+            <Link to="/client">Nasze kina</Link>
+          </li>
+          <li>
+            <Link className="active" to="/client/login/reservations">
+              Bilety
+            </Link>
+          </li>
+          <li>
+            <Link to="/client/login/userdata">Moje konto</Link>
+          </li>
+        </ul>
+      </div>
       <Row className="bg-light p-4 rounded shadow">
         {/* Formularz danych osobowych */}
         <Col md={6}>
